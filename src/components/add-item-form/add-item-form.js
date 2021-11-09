dca801b068a1c6ac760ca8d7670b90da245d4484
Valentyn <valantg@gmail.com>
2021-11-09 15:58:07 +0200

    add clear form

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -19,6 +19,9 @@ export default class AddItemForm extends Component {
     this.onSubmit = (e) => {
       e.preventDefault();
       this.props.addItem(this.state.label);
+      this.setState({
+        label: "",
+      });
     };
   }
 
@@ -30,6 +33,7 @@ export default class AddItemForm extends Component {
           className="form-control"
           onChange={this.onLabelChange}
           placeholder="What needs to be done"
+          value={this.state.label}
         />
         <button className="btn btn-outline-secondary">Add Item</button>
       </form>
